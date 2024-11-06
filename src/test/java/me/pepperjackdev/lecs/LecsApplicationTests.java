3dabd275799ca8fc65f127bcb85773050250810b
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-11-06 21:18:13 +0100

    Getting ready for tests

--- src/test/java/me/pepperjackdev/lecs/LecsApplicationTests.java
+++ src/test/java/me/pepperjackdev/lecs/LecsApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LecsApplicationTests {
+public class LecsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
+    
 }
---
+++ src/test/java/me/pepperjackdev/lecs/controller/SubjectControllerTest.java
@@ -0,0 +1,5 @@
+package me.pepperjackdev.lecs.controller;
+
+public class SubjectControllerTest {
+    
+}
