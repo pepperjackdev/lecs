ee15318bb14dfb75770ade432b0887e44dc2f4d6
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2024-11-09 21:05:34 +0100

    Written tests for SubjectRepository

--- src/test/java/me/pepperjackdev/lecs/subject/SubjectRepositoryTest.java
+++ src/test/java/me/pepperjackdev/lecs/subject/SubjectRepositoryTest.java
@@ -1,5 +1,29 @@
 package me.pepperjackdev.lecs.subject;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
 public class SubjectRepositoryTest {
-    
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Test
+    void shouldFindSubjectByTitle() {
+        // given 
+        Subject subject = new Subject(1L, "Test Subject");
+        subjectRepository.save(subject);
+
+        // when
+        Optional<Subject> result = subjectRepository.findByTitle("Test Subject");
+
+        // then
+        assertThat(result).isPresent();
+    }
 }
